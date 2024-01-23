3986147a98857da662cad9c07749f21c9cacb1b4
itc-s23029 <s23029@std.it-college.ac.jp>
2024-01-23 06:59:01 +0000

    1syou
    １章おわた

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+const Home = () => {
+  return (
+    <>
+      <header>HEADER</header>
+
+      <main>
+        <div>
+          <h1>CUBE</h1>
+          <p>アウトプットしていくサイト</p>
+        </div>
+      </main>
+
+      <footer>FOOTER</footer>
+    </>
+  )
+}
+
+export default Home
