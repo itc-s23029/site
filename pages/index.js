d07f7062849ff755a894951df90865aafc0f85b6
itc-s23029 <s23029@std.it-college.ac.jp>
2024-01-24 05:49:24 +0000

    2.9
    ２．９おわた

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Hero from 'components/hero'
 
 const Home = () => {
-  return <Hero title='CUBE' subtitle='アウトプットしていくサイト' />
+  return <Hero title='CUBE' subtitle='アウトプットしていくサイト' imageOn />
 }
 
 export default Home
