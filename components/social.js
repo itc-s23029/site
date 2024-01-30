a30c4efbea4d645a9632faad091207a5a3a9b6d9
itc-s23029 <s23029@std.it-college.ac.jp>
2024-01-30 07:02:15 +0000

    5.9
    ５．９章
      アイコンを使ってソーシャルリンクメニューを作成する

---
+++ components/social.js
@@ -0,0 +1,35 @@
+import styles from 'styles/social.module.css'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faTwitter,
+  faFacebookF,
+  faGithub
+} from '@fortawesome/free-brands-svg-icons'
+
+const Social = () => {
+  return (
+    <ul className={styles.list}>
+      <li>
+        <span href='https://twitter.com/'>
+          <FontAwesomeIcon icon={faTwitter} />
+          <span className='sr-only'>Twitter</span>
+        </span>
+      </li>
+      <li>
+        <span href='https://www.facebook.com/'>
+          <FontAwesomeIcon icon={faFacebookF} />
+          <span className='sr-only'>Facebook</span>
+        </span>
+      </li>
+      <li>
+        <span href='https://github.com/'>
+          <FontAwesomeIcon icon={faGithub} />
+          <span className='sr-only'>GitHub</span>
+        </span>
+      </li>
+    </ul>
+  )
+}
+
+export default Social
