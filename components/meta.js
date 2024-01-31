b3ca8835fa7c8d2ceeaddd0bc9f5fe176cd87e04
itc-s23029 <s23029@std.it-college.ac.jp>
2024-01-31 05:45:02 +0000

    6.4
    ６．４章
      サイトに関する情報を共有できる形で用意する

--- components/meta.js
+++ components/meta.js
@@ -1,10 +1,20 @@
 import Head from 'next/head'
+import { siteMeta } from 'lib/constants'
+const {
+  siteTitle,
+  siteDesc,
+  siteUrl,
+  siteLocale,
+  siteType,
+  siteIcon
+} = siteMeta
 
 const Meta = ({ pageTitle }) => {
+  const title = pageTitle ? `${pageTitle} | ${siteTitle}` : siteTitle
   return (
     <Head>
-      <title>{pageTitle} </title>
-      <meta property='og:title' content={pageTitle} />
+      <title>{title}</title>
+      <meta property='og:title' content={title} />
     </Head>
   )
 }
