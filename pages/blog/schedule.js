a752b92ebe94098e79b039f5149e890341088777
itc-s23029 <s23029@std.it-college.ac.jp>
2024-02-02 03:34:08 +0000

    8.4
    ８．４章
      記事の本文を表示する

--- pages/blog/schedule.js
+++ pages/blog/schedule.js
@@ -1,6 +1,13 @@
 import { getPostBySlug } from 'lib/api'
 import Container from 'components/container'
 import PostHeader from 'components/post-header'
+import PostBody from 'components/post-body'
+import {
+  TwoColumn,
+  TwoColumnMain,
+  TwoColumnSidebar
+} from 'components/two-column'
+import ConvertBody from 'components/convert-body'
 import Image from 'next/image'
 
 export default function Schedule ({
@@ -22,10 +29,19 @@ export default function Schedule ({
             layout='responsive'
             width={eyecatch.width}
             height={eyecatch.height}
-            sizes='(min-width: 1152px) 1152px, 100nw'
+            sizes='(min-width: 1152px) 1152px, 100vw'
             priority
           />
         </figure>
+
+        <TwoColumn>
+          <TwoColumnMain>
+            <PostBody>
+              <ConvertBody contentHTML={content} />
+            </PostBody>
+          </TwoColumnMain>
+          <TwoColumnSidebar></TwoColumnSidebar>
+        </TwoColumn>
       </article>
     </Container>
   )
