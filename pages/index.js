3a95dba3100b998ea1078877466b847a03f9c23a
itc-s23029 <s23029@std.it-college.ac.jp>
2024-02-21 06:12:08 +0000

    9.7
    ９．７章
      トップページに記事一覧を表示する

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,39 @@
-import Meta from 'components/meta'
+import { getAllPosts } from 'lib/api'
 import Container from 'components/container'
 import Hero from 'components/hero'
+import Posts from 'components/posts'
+import Pagination from 'components/pagination'
+import { getPlaiceholder } from 'plaiceholder'
+import { eyecatchLocal } from 'lib/constants'
 
-const Home = () => {
+const Home = ({ posts }) => {
   return (
     <Container>
-      <Meta />
       <Hero title='CUBE' subtitle='アウトプットしていくサイト' imageOn />
+      <Posts posts={posts} />
+      <Pagination nextUrl='/blog' nextText='More Posts' />
     </Container>
   )
 }
 
 export default Home
+
+const getStaticProps = async () => {
+  const posts = await getAllPosts(4)
+
+  for (const post of posts) {
+    if (!Object.prototype.hasOwnProperty.call(post, 'eyecatch')) {
+      post.eyecatch = eyecatchLocal
+    }
+    const { base64 } = await getPlaiceholder(post.eyecatch.url)
+    post.eyecatch.blurDataURL = base64
+  }
+
+  return {
+    props: {
+      posts
+    }
+  }
+}
+
+export { getStaticProps }
