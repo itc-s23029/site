358ca1bca792cfeb799d2d0a230d372cda776ddf
itc-s23029 <s23029@std.it-college.ac.jp>
2024-02-22 07:12:04 +0000

    10.7
    １０．７章
      uesEffectを使ってGoogleアナリティクスを設定する

---
+++ pages/404.js
@@ -0,0 +1,14 @@
+import Meta from 'components/meta'
+import Container from 'components/container'
+import Hero from 'components/hero'
+
+const Custom404 = () => {
+  return (
+    <Container>
+      <Meta pageTitle='404 - Page not  found' />
+      <Hero title='404' subtitle='ページが見つかりません' />
+    </Container>
+  )
+}
+
+export { Custom404 }
